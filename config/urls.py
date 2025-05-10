ec778182a85d3d8c252b0254b706de87141212c3
vahid rajabi <vahidrajabi.software@gmail.com>
2025-05-10 09:47:19 +0330

    Configure project URLs for Djoser (JWT, social), debug toolbar, and static/media file handling during development

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,19 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
+    # Authentications Urls
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
+    path("auth/", include("djoser.social.urls")),
+    path("auth/o/", include("social_django.urls", namespace="social")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
