1792b5c397ffc6af97a3b7f9dab0997cde61b0f4
vahid rajabi <vahidrajabi.software@gmail.com>
2025-05-10 09:32:41 +0330

    Implement custom User model named 'User' using AbstractBaseUser, define UserManager, apply migrations, and configure AUTH_USER_MODEL

--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+        max_length=255,
+    )
+    username = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+    first_name = models.CharField(
+        max_length=50,
+    )
+    last_name = models.CharField(
+        max_length=50,
+    )
+    phone_number = models.CharField(max_length=11, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = [
+        "username",
+        "first_name",
+        "last_name",
+        "phone_number",
+    ]
+
+    objects = UserManager()
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return f"{self.first_name} | {self.email}"
