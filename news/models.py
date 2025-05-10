b7349e3276c9fedeb8263eae4551adcacc898c02
vahid rajabi <vahidrajabi.software@gmail.com>
2025-05-10 11:31:59 +0330

    Define Tag and News models with M2M relationship; register both in admin panel

--- news/models.py
+++ news/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="news")
+    source = models.CharField(max_length=255)
+    datatime_created = models.DateTimeField(auto_now_add=True)
+    datatime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
