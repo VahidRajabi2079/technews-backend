1e28173d97e7b8aac5daaaf28deaef3560a9f8a7
vahid rajabi <vahidrajabi.software@gmail.com>
2025-05-10 09:34:00 +0330

    Register custom User model in Django admin and customize user display and form fieldsets

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,97 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+@admin.register(get_user_model())
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    list_display = [
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "phone_number",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    ]
+    search_fields = [
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+    ]
+
+    readonly_fields = ("date_joined", "last_login")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Personal Info"),
+            {
+                "fields": (
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "phone_number",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            _("Important dates"),
+            {"fields": ("last_login", "date_joined")},
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            _("User Information"),
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "phone_number",
+                ),
+            },
+        ),
+        (
+            _("Password"),
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
