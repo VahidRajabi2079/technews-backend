7e927283bf899944a50b02dfe153ef657e58bb84
vahid rajabi <vahidrajabi.software@gmail.com>
2025-05-10 16:07:56 +0330

    add datatime_modified file in serrializers

--- news/serializers.py
+++ news/serializers.py
@@ -5,17 +5,47 @@ from . import models
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Tag
-        fields = ["id", "name"]
+        fields = [
+            "id",
+            "name",
+        ]
 
 
 class NewsSerializer(serializers.ModelSerializer):
-    tags = serializers.SlugRelatedField(
-        slug_field="name", 
-        queryset=models.Tag.objects.all(), 
-        many=True
+    tags = TagSerializer(
+        many=True,
+        read_only=True,
+    )
+    tag_ids = serializers.PrimaryKeyRelatedField(
+        queryset=models.Tag.objects.all(),
+        many=True,
+        write_only=True,
     )
 
     class Meta:
         model = models.News
-        fields = ["id", "title", "body", "tags", "source", "datatime_created"]
+        fields = [
+            "id",
+            "title",
+            "body",
+            "tags",
+            "tag_ids",
+            "source",
+            "datatime_created",
+            "datatime_modified",
+        ]
 
+    def create(self, validated_data):
+        tag_ids = validated_data.pop("tag_ids")
+        news = models.News.objects.create(**validated_data)
+        news.tags.set(tag_ids)
+        return news
+
+    def update(self, instance, validated_data):
+        tag_ids = validated_data.pop("tag_ids", None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        if tag_ids is not None:
+            instance.tags.set(tag_ids)
+        return instance
